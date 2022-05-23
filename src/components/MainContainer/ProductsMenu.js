487b4565388cc0c998a4d198986501aec91bb6c0
AtwijukireBrendah <kire5brenda@gmail.com>
2022-05-23 23:57:57 +0300

    displaying items in cart

--- src/components/MainContainer/ProductsMenu.js
+++ src/components/MainContainer/ProductsMenu.js
@@ -1,11 +1,17 @@
 import React from "react"
 
-function ProductsMenu(props){
-    return (<ul style={{ listStyle: 'none' }}>
-        {
-            props.categories.map((category, index) => <li key={index} onClick={() => { props.selectProductCategory(index) }}>{category}</li>)
-        }
-    </ul>)
+function ProductsMenu(props) {
+    return (
+        <span className="productsmenu">
+            <ul class="phone-brands-list" style={{ listStyle: 'none' }}>
+                {
+                    props.categories.map((category, index) =>
+                        <h1><li class="phone-brand" key={index} onClick={() => { props.selectProductCategory(index) }}><span>{category}</span></li></h1>)
+                }
+            </ul>
+       
+    </span >
+    )
 }
 
 export default ProductsMenu
